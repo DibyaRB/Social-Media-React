905e784ffded3c4760a2ee15292853068cd664d0
Dibya Ranjan Bhoi <dibyabhoidibya@gmail.com>
2021-12-13 23:10:00 +0530

    Finishing Feed and Post Component

--- src/components/feed/Feed.js
+++ src/components/feed/Feed.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import './Feed.css'
+import Share from '../share/Share'
+import Post from '../post/Post'
+
 
 function Feed() {
     return (
         <div className='feed'>
-            feed
+            <div className="feedWrapper">
+                <Share/>
+                <Post/>
+                <Post/>
+                <Post/>
+                <Post/>
+                <Post/>
+                <Post/>
+            </div>
         </div>
     )
 }
