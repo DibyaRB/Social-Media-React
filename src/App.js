32536e85009e8beca947a0c685672253bb883f53
Dibya Ranjan Bhoi <dibyabhoidibya@gmail.com>
2021-12-19 13:11:12 +0530

    Finishing login page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Home from './pages/home/Home'
 import Profile from './pages/profile/Profile';
+import Login from './pages/login/Login'
 
 
 function App() {
@@ -8,7 +9,8 @@ function App() {
     <div>
     
      {/* <Home/> */}
-     <Profile/>
+     {/* <Profile/> */}
+     <Login/>
     </div>
   );
 }
