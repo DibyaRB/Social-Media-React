f4e7c810ddc5fd1625381e4348119def53dae48b
Dibya Ranjan Bhoi <dibyabhoidibya@gmail.com>
2021-12-19 12:54:19 +0530

    Finishing profile component.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Home from './pages/home/Home'
+import Profile from './pages/profile/Profile';
 
 
 function App() {
   return (
     <div>
     
-     <Home/>
+     {/* <Home/> */}
+     <Profile/>
     </div>
   );
 }
